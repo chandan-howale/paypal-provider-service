ec702bad7f777d37870c031845dd3f41fc87b528
chandan-howale-admin <howalechandan@gmail.com>
2026-05-17 16:14:26 +0530

    fix: - as i added .env file it didnt load with spring context thsts why added this dependency - local-properties fixing the variaible name - as i use .env it didnt pass the test cases that why this update in test file

--- src/test/java/com/chandan/payments/PaypalProviderServiceApplicationTests.java
+++ src/test/java/com/chandan/payments/PaypalProviderServiceApplicationTests.java
@@ -2,12 +2,16 @@ package com.chandan.payments;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@TestPropertySource(properties = {
+    "paypal.client.id=test",
+    "paypal.client.secret=test"
+})
 class PaypalProviderServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+	
+    @Test
+    void contextLoads() {
+    }
 }
